2322d94e3c0cc545250fe789ed989f16daa9da06
basavaraj1997 <48676178+basavaraj1997@users.noreply.github.com>
2024-06-02 00:47:46 +0530

    new changes updated

--- pdf_text_table_extract not mixed/pdf_table_text.py
+++ pdf_text_table_extract not mixed/pdf_table_text.py
@@ -1,3 +1,4 @@
+import json
 import pdfplumber
 from tabula import read_pdf
 import camelot
@@ -6,50 +7,67 @@ import pandas as pd
 def extract_text_and_tables(pdf_path):
 
   with pdfplumber.open(pdf_path) as pdf:
-    text_outside_tables = []
+    text_outside_tables1 = []
     tables = []
+    text_inSequence=[]
 
-    for page in pdf.pages:            
+    for page in pdf.pages:
+      text_outside_tables = []            
       all_elements = page.extract_text(x0=0, y0=0, x1=page.width, y1=page.height)
-      elements = all_elements.split("\n")
+      elements = all_elements.split("\n")      
 
       col2str = {'dtype': str}
       kwargs = {
             'output_format': 'dataframe',
             'pandas_options': col2str,
-            'stream': True,           
-            'multiple_tables':  True,
+            'guess': True,                       
             'encoding':'utf-8',
+            'force_subprocess':False,
+            'relative_area':False
             }
 
-      tables = read_pdf(pdf_path,pages="1",**kwargs)
+      tables = read_pdf(pdf_path,pages=page.page_number,multiple_tables=True,**kwargs)
       print('tables: ',len(tables))
+      print(tables)
       extracted_tables = pd.concat(tables, ignore_index=True).map(str)
 
       text_outside_tables = []
+      is_last_row=False
       for element in elements:
         found = False
-        
+
+        if is_last_row:
+           text_inSequence.append(element.strip())
+           continue
+           
+        i=0
         for row in extracted_tables.itertuples(index=False):
-            
+            i=i+1 
             fulltext=''
             for cell in row:
+               cell=cell.replace('NaN','').replace('nan','')
                fulltext=fulltext+cell.replace('NaN','').replace('nan','').replace(r'^Unnamed.*$', '')
                         
             if fulltext.replace(' ', '') == element.replace(' ', '') :
                 found = True
-                print('\n \n *********************matched***************** \n \n')                
+                print('\n \n *********************matched***************** \n \n') 
+                if (len(extracted_tables.values))==i:
+                   is_last_row=True
                 break
-
+        if is_last_row:
+           for line in text_outside_tables:
+              text_inSequence.append(line)
+           text_inSequence.append(json.dumps(json.loads(extracted_tables.to_json(orient="records"))))
         if not found:
             text_outside_tables.append(element.strip())
+            text_outside_tables1.append(element.strip())
 
-  return text_outside_tables, tables
+            
+  return text_inSequence
   
-text_outside_tables, tables = extract_text_and_tables("6.pdf")
-for line in text_outside_tables:
-  print(line)
+text_inSequence = extract_text_and_tables("5.pdf")
 
-print("\nExtracted tables:")
-for table in tables:
-  print(table)
+print("\nExtracted Text, tables:")
+for text_table in text_inSequence:
+  print(text_table)
+ 
